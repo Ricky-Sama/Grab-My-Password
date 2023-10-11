aeaf02812e212cc20dc9755ba66667208ae8e4d6
Shamar MELVIN <shamar@MacBook-Pro.local>
2023-10-10 21:19:29 -0400

    login/signup

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const User = require('../../models/User');
+
+//route to create/add a User using async await
+// router.post('/', async (req, res) => {
+//     try {
+//         const userData = await User.create(req.body);
+//         req.session.save(() => {
+//             req.session.user_id = userData.id;
+//             req.session.logged_in = true;
+//             res.status(200).json(userData);
+//         });
+//     } catch (err) {
+//         res.status(400).json(err);
+//     }
+// });
+
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create({
+            user: req.body.user,
+            email: req.body.email,
+            password: req.body.password
+        });
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
