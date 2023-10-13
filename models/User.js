b3a87d087aa8bd38dbfbeec8c9427c4d93a8ff11
Shamar MELVIN <shamar@MacBook-Pro.local>
2023-10-12 21:06:42 -0400

    //out

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
+
 class User extends Model {
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
@@ -55,10 +56,10 @@ User.init(
   }
 );
 
-User.hasMany(Website, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE',
-});
+// User.hasMany(Website, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE',
+// });
 
 
 module.exports = User;
