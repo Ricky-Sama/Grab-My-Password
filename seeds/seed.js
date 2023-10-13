364cab865f221f0f13e1e6a897b96bbec3a9bb4a
Kevin Rice <reatriniti@gmail.com>
2023-10-12 20:00:00 -0400

    a lot of seeds

---
+++ seeds/seed.js
@@ -0,0 +1,19 @@
+const seedUser = require('./user-seeds');
+const seedWebsite = require('./website-seeds');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----\n');
+    await seedUser();
+    console.log('\n----- USER SEEDED -----\n');
+  
+    await seedWebsite();
+    console.log('\n----- WEBSITE SEEDED -----\n');
+  
+  
+    process.exit(0);
+  };
+  
+  seedAll();
